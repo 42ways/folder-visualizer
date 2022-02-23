e3f869bbaded71ae6dd6572b39a8759902f00a82
Thomas Herrmann <mail@thoherr.de>
2022-02-23 23:53:11 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+# Traverse a directory tree and create an PlantUML source file to visualize it
+import sys
+import os
+
+
+def visualise_tree(filename, name):
+    with open(filename, 'w') as output:
+        try:
+            output.writelines('@startuml\n')
+            traverse_tree(output, name)
+            output.write('@enduml\n')
+        finally:
+            output.close()
+
+
+def canonical_name(name):
+    return name.replace("/", "_").replace("-", "_")
+
+
+def traverse_tree(output, name):
+    for dirpath, dirnames, filenames in os.walk(name):
+        output.write(f'folder {canonical_name(dirpath)} as "{os.path.basename(dirpath)}"\n')
+        if len(os.path.dirname(dirpath)) > 0:
+            output.write(f'{canonical_name(os.path.dirname(dirpath))} --- {canonical_name(dirpath)}\n')
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        name = sys.argv[1]
+    else:
+        name = "."
+    visualise_tree('folder-tree.puml', name)
